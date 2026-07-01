c11755b77bafbe4949ce884a2132ee6cba661838
rhythmjain282 <jainrhythm191@gmail.com>
2026-07-01 17:04:53 +0530

    Time: 15 ms (94.06%) | Memory: 70.7 MB (99.27%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,27 +1,7 @@
 class Solution {
-private:
-int pindex(vector<int>&nums,int low,int high){
-    int left = low;
-    int right = high;
-    int pivot = nums[low];
-    while(left<right){
-        while(nums[left]<=pivot && left<=high-1) left++;
-        while(nums[right]>pivot && right>=low+1) right--;
-        if(left<right)swap(nums[left],nums[right]);
-    }
-    swap(nums[right],nums[low]);
-    return right;
-}
-    void qs(vector<int>&nums,int low,int high){
-        if(low<high){
-            int ind = pindex(nums,low,high);
-            qs(nums,low,ind-1);
-            qs(nums,ind+1,high);
-        }
-    }
 public:
     vector<int>sortArray(vector<int>&nums){
-        qs(nums,0,nums.size()-1);
+        sort(nums.begin(),nums.end());
         return nums;
     }
 };
